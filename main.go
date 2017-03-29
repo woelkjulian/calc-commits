7752ab3cdcb5a19a7832f9dc35ee4df992df9046
Julian Woelk <jwo@adorsys.de>
2017-03-29 14:17:36 +0200

    minor changes

--- main.go
+++ main.go
@@ -12,20 +12,23 @@ import (
 )
 
 // Configurable as flag??
+// per_page max value is 100
 var perPage = 100
 
 func main() {
+
 	urlPtr := flag.String("url", "", "gitlab url with the projects (e.g. https://gitlab.example.com)")
-	projNamePtr := flag.String("projName", "", "namespace/projectname of the gitlab project")
-	projIDPtr := flag.String("projId", "", "instead of namespace/projectname, id of the gitlab project (e.g. 999)")
+	projNamePtr := flag.String("projname", "", "namespace/projectname of the gitlab project")
+	projIDPtr := flag.String("projid", "", "instead of namespace/projectname, id of the gitlab project (e.g. 999)")
 	tokenPtr := flag.String("t", "", "private token of gitlab user (e.g. abcdefg123456)")
+	apiVersionPtr := flag.String("vapi", "3", "version of gitlab api")
 	branchPtr := flag.String("b", "", "optional name of git branch (e.g. master)")
 	logPtr := flag.Bool("v", false, "optional additional log info")
 
 	flag.Parse()
 
 	if *urlPtr == "" || *tokenPtr == "" || (*projIDPtr == "" && *projNamePtr == "") {
-		msg := "Arguments missing: \n"
+		msg := "Required arguments missing: \n"
 
 		if *urlPtr == "" {
 			msg += "- URL (-url https://gitlab.example.com)\n"
@@ -41,12 +44,12 @@ func main() {
 		return
 	}
 
-	if err := calcMergeCommitsQuotient(urlPtr, tokenPtr, branchPtr, projNamePtr, projIDPtr, logPtr); err != nil {
-		fmt.Printf("Error while calculating: %v\n", err)
+	if err := calcMergeCommitsAmount(urlPtr, tokenPtr, branchPtr, projNamePtr, projIDPtr, apiVersionPtr, logPtr); err != nil {
+		fmt.Printf("calcMergeCommitsQuotient(): Error: %v\n", err)
 	}
 }
 
-func calcMergeCommitsQuotient(pURL, pToken, pBranch, pProjName, pProjID *string, pLog *bool) error {
+func calcMergeCommitsAmount(pURL, pToken, pBranch, pProjName, pProjID, pVApi *string, pLog *bool) error {
 
 	var noOfMergeCommits int
 	var pProj *string
@@ -58,7 +61,7 @@ func calcMergeCommitsQuotient(pURL, pToken, pBranch, pProjName, pProjID *string,
 		pProj = pProjID
 	}
 
-	commits, err := getAllCommits(pURL, pToken, pBranch, pProj, 0, pLog)
+	commits, err := getAllCommits(pURL, pToken, pBranch, pProj, pVApi, 0, pLog)
 
 	if err != nil {
 		return err
@@ -66,7 +69,7 @@ func calcMergeCommitsQuotient(pURL, pToken, pBranch, pProjName, pProjID *string,
 
 	fmt.Printf("\n\nTotal number of Commits = %v\n\n", len(commits))
 
-	mergeRequests, err := getMergeRequests(pURL, pToken, pBranch, pProj, 0, pLog)
+	mergeRequests, err := getMergeRequests(pURL, pToken, pBranch, pProj, pVApi, 0, pLog) // 0 for start at page=0
 
 	if err != nil {
 		return err
@@ -75,7 +78,7 @@ func calcMergeCommitsQuotient(pURL, pToken, pBranch, pProjName, pProjID *string,
 	fmt.Printf("\n\nTotal number of Merge Requests = %v\n\n", len(mergeRequests))
 
 	for _, req := range mergeRequests {
-		mergeRequestCommits, err := getMergeRequestCommits(pURL, pToken, pBranch, pProj, req.Id, 0, pLog)
+		mergeRequestCommits, err := getMergeRequestCommits(pURL, pToken, pBranch, pProj, pVApi, req.Id, 0, pLog)
 
 		if err != nil {
 			return err
@@ -85,7 +88,6 @@ func calcMergeCommitsQuotient(pURL, pToken, pBranch, pProjName, pProjID *string,
 	}
 
 	fmt.Printf("\n\nTotal number of Merge Request Commits = %v\n\n", noOfMergeCommits)
-
 	fmt.Printf("\n\nPercentage of Merge Request Commits: %.2f%v\n\n",
 		float64(noOfMergeCommits)/float64(len(commits))*float64(100),
 		"%")
@@ -93,16 +95,15 @@ func calcMergeCommitsQuotient(pURL, pToken, pBranch, pProjName, pProjID *string,
 	return nil
 }
 
-func getAllCommits(pURL, pToken, pBranch, pProj *string, page int, pLog *bool) ([]model.Commit, error) {
+func getAllCommits(pURL, pToken, pBranch, pProj, pVApi *string, page int, pLog *bool) ([]model.Commit, error) {
 
-	var url = fmt.Sprint(*pURL, "/api/v3/projects/", *pProj, "/repository/commits")
+	var url = fmt.Sprint(*pURL, "/api/v", *pVApi, "/projects/", *pProj, "/repository/commits")
 
 	if *pBranch != "" {
-		branch := "%2F:" + *pBranch
+		branch := "/:" + *pBranch
 		url += branch
 	}
 
-	// per_page max is 100
 	url += fmt.Sprint("?per_page=", perPage, "&page=", page)
 
 	if *pLog == true {
@@ -131,7 +132,7 @@ func getAllCommits(pURL, pToken, pBranch, pProj *string, page int, pLog *bool) (
 		if *pLog == true {
 			fmt.Printf("\ngetCommits(): found more than %v commits on page %v, searching next page...\n", perPage, page)
 		}
-		newCommits, err := getAllCommits(pURL, pToken, pBranch, pProj, page+1, pLog)
+		newCommits, err := getAllCommits(pURL, pToken, pBranch, pProj, pVApi, page+1, pLog)
 
 		if err != nil {
 			return nil, err
@@ -145,12 +146,12 @@ func getAllCommits(pURL, pToken, pBranch, pProj *string, page int, pLog *bool) (
 	return commits, nil
 }
 
-func getMergeRequests(pURL, pToken, pBranch, pProj *string, page int, pLog *bool) ([]model.MergeRequest, error) {
+func getMergeRequests(pURL, pToken, pBranch, pProj, pVApi *string, page int, pLog *bool) ([]model.MergeRequest, error) {
 
-	var url = fmt.Sprint(*pURL, "/api/v3/projects/", *pProj, "/merge_requests")
+	var url = fmt.Sprint(*pURL, "/api/v", *pVApi, "/projects/", *pProj, "/merge_requests")
 
 	if *pBranch != "" {
-		branch := "%2F:" + *pBranch
+		branch := "/:" + *pBranch
 		url += branch
 	}
 
@@ -183,7 +184,7 @@ func getMergeRequests(pURL, pToken, pBranch, pProj *string, page int, pLog *bool
 		if *pLog == true {
 			fmt.Printf("\ngetMergeRequests(): found more than %v mergeRequests on page %v, searching next page...\n", perPage, page)
 		}
-		newMergeRequests, err := getMergeRequests(pURL, pToken, pBranch, pProj, page+1, pLog)
+		newMergeRequests, err := getMergeRequests(pURL, pToken, pBranch, pProj, pVApi, page+1, pLog)
 
 		if err != nil {
 			return nil, err
@@ -197,12 +198,12 @@ func getMergeRequests(pURL, pToken, pBranch, pProj *string, page int, pLog *bool
 	return mergeRequests, nil
 }
 
-func getMergeRequestCommits(pURL, pToken, pBranch, pProj *string, mergeReqID int, page int, pLog *bool) ([]model.Commit, error) {
+func getMergeRequestCommits(pURL, pToken, pBranch, pProj, pVApi *string, mergeReqID int, page int, pLog *bool) ([]model.Commit, error) {
 
-	var url = fmt.Sprint(*pURL, "/api/v3/projects/", *pProj, "/merge_requests/", mergeReqID, "/commits")
+	var url = fmt.Sprint(*pURL, "/api/v", *pVApi, "/projects/", *pProj, "/merge_requests/", mergeReqID, "/commits")
 
 	if *pBranch != "" {
-		branch := "%2F:" + *pBranch
+		branch := "/:" + *pBranch
 		url += branch
 	}
 
@@ -236,7 +237,7 @@ func getMergeRequestCommits(pURL, pToken, pBranch, pProj *string, mergeReqID int
 		if *pLog == true {
 			fmt.Printf("\ngetMergeRequestCommits(): found more than %v merge request commits on page %v, searching next page...\n", perPage, page)
 		}
-		newMergeRequestCommits, err := getMergeRequestCommits(pURL, pToken, pBranch, pProj, mergeReqID, page+1, pLog)
+		newMergeRequestCommits, err := getMergeRequestCommits(pURL, pToken, pBranch, pProj, pVApi, mergeReqID, page+1, pLog)
 
 		if err != nil {
 			return nil, err
